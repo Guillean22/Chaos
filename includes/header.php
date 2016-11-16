efad4ef797641e9a0ad92c2376228e614fd878de
Guillean22 <jean.rodrigue.stella@gmail.com>
2016-11-16 12:06:02 +0100

    création d'une 2ème page + création d'un menu dans le header

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,4 @@
 		<header>
-		
+			<a href='../index.php'>Accueil</a>
+			<a href='../pages/equipe.php'>L'équipe</a>
 		</header>
---
+++ pages/equipe.php
@@ -0,0 +1,18 @@
+<?php require '../includes/head.php'; ?>
+	<body>
+		<?php require '../includes/header.php'; ?>
+		<div>
+			<h1><u>LA TEAM CHAOS</u></h1>
+			<h2><br />Mothinos</h2>
+			<p>Thomas Trotin</p>
+			<h2><br />Xandir59</h2>
+			<p>Dylan Engels</p>
+			<h2><br />AlanGal</h2>
+			<p>Alan Gal</p>
+			<h2><br />DoonMecha</h2>
+			<p>Constantin Rioche</p>
+			<h2><br />Guillean</h2>
+			<p>Jean-Rodrigue Stella</p>
+		</div>
+	</body>
+	<?php require '../includes/footer.php'; ?>
