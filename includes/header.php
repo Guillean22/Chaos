89f36d9ec8494e538814a98925586aa0ddf5296c
Guillean22 <jean.rodrigue.stella@gmail.com>
2016-11-16 12:17:21 +0100

    no message

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
 		<header>
-			<a href='/Chaos/index.php'>Accueil</a>
-			<a href='/Chaos/pages/equipe.php'>L'équipe</a>
+			<a href='/Projets/Chaos/index.php'>Accueil</a>
+			<a href='/Projets/Chaos/pages/equipe.php'>L'équipe</a>
 		</header>
