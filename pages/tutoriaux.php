c2bc30f04bc9f893b2c95df65399cb676af42d0c
Doon <petersteele@hotmail.fr>
2016-11-16 16:12:15 +0100

    Added tutoriaux.php

---
+++ pages/tutoriaux.php
@@ -0,0 +1,8 @@
+<?php require 'includes/head.php'; ?>
+	<body>
+		<?php require 'includes/header.php'; ?>
+		<div>
+			<p>Hello World</p>
+		</div>
+	</body>
+	<?php require 'includes/footer.php'; ?>
