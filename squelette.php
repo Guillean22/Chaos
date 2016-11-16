7505e9edf0ac40a2f7a839c6e644b897ef526f7c
Guillean22 <jean.rodrigue.stella@gmail.com>
2016-11-16 16:15:27 +0100

    correction du squelette

--- squelette.php
+++ squelette.php
@@ -1,8 +1,8 @@
-<?php require 'includes/head.php'; ?>
+<?php require '../includes/head.php'; ?>
 	<body>
-		<?php require 'includes/header.php'; ?>
+		<?php require '../includes/header.php'; ?>
 		<div>
 		<!-- Votre contenu ici ! -->
 		</div>
 	</body>
-	<?php require 'includes/footer.php'; ?>
+	<?php require '../includes/footer.php'; ?>
