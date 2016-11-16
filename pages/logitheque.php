c14495144fc16a634e04ae292073b60b8bfde58a
Guillean22 <jean.rodrigue.stella@gmail.com>
2016-11-16 16:29:58 +0100

    clean du style2.css

--- pages/logitheque.php
+++ pages/logitheque.php
@@ -4,28 +4,24 @@
 		<div>
 			<table id='TableSoft'>
 				<tr>
-					<td>OS</td>
 					<td>Logiciel</td>
-					<td>32/64</td>
+					<td>OS</td>
 					<td>Lien</td>
 					<td>Date</td>
 				</tr>
 				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
+					<td>Sublime Text 3</td>
+					<td>Windows/Linux</td>
+					<td><a href='https://www.sublimetext.com/3'>Link here</a></td>
+					<td>16/11/2016</td>
 				</tr>
 				<tr>
 					<td></td>
 					<td></td>
 					<td></td>
 					<td></td>
-					<td></td>
 				</tr>
 				<tr>
-					<td></td>
 					<td></td>
 					<td></td>
 					<td></td>
