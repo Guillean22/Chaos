92439169a44ce833021107cb6f4c8c233ddbaf38
Guillean22 <jean.rodrigue.stella@gmail.com>
2016-11-16 13:37:07 +0100

    no message

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,12 @@
 		<header>
-			<a href='/Projets/Chaos/index.php'>Accueil</a>
-			<a href='/Projets/Chaos/pages/equipe.php'>L'équipe</a>
+			<table id='menuhorizontal'>
+				<tr>
+					<td>
+						<a href='/Projets/Chaos/index.php'>Accueil</a>
+					</td>
+					<td>
+						<a href='/Projets/Chaos/pages/equipe.php'>L'équipe</a>
+					</td>
+				</tr>
+			</table>
 		</header>
