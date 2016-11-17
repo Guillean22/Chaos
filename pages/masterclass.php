babec4ee50a4843ca73f6308efd0f0e927d921fb
mothinos <trotin.thomas@gmail.com>
2016-11-17 14:53:23 +0100

    amélioration de l'article L-A

--- pages/masterclass.php
+++ pages/masterclass.php
@@ -8,28 +8,39 @@
 					<a href="#Marine"><li>Marine Dautrème</li></a>
 <div id="ludovic">
 <a href="https://twitter.com/NkL4"><img src="https://pbs.twimg.com/profile_images/770207539296436224/p6s80Pzh_400x400.jpg"></a>
-<img title="Matrice" src="http://www.la-matrice.org/">
-		<ul>
-			<li>Ludovic Arnold</li>
-			<li>fonction: Fondateur/bienfaiteur</li>
-			<li>Organisme:
+		<img title="Matrice" src="http://www.la-matrice.org/wp-content/uploads/2013/05/logo-icononly.png">
+			<article>
 				<ul>
-			 		<li>La:Matrice</li>
-			 		<li>Kreizenn Dafar</li>
-			 	</ul>
-			 </li>
-			 <li>rencontre du 4 Novembre 2016</li>
-		</ul>
-				<h2>Ludovic Arnorld ou comment je suis devenu une <strong>méduse</strong> (:~</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<p>
-
-	</ul>
+					<li>Ludovic Arnold</li>
+					<li>fonction: Fondateur/bienfaiteur</li>
+					<li>Organisme:
+						<ul>
+					 		<li>La:Matrice</li>
+					 		<li>Kreizenn Dafar</li>
+					 	</ul>
+					 </li>
+					 <li>rencontre du 4 Novembre 2016</li>
+				</ul>
+						<h2>Ludovic Arnorld ou comment je suis devenu une <strong>méduse</strong> (:~</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<h2>Rensegnez-vous!</h2>
+							<p>DATALOVE <3</p>
+							<P>Meat Space</P>
+							<p>telecomix</p>
+							<p>Benjamin Bayar: <a href="https://www.youtube.com/watch?v=HqkcCC8121Q">minitel 2.0</a></p>
+							<p>Fidh</p>
+							<p>Fabrice Epelboin</p>
+							<p>LeanStartup</p>
+							<p>BMC</p>
+							<p>la quadrature du net</p>
+							<p>méthode agile</p>
+				</ul>
+		</article>
 	</br>
 	<div id="Marine">
 <a href="https://twitter.com/marinedautreme"><img src="https://pbs.twimg.com/profile_images/798157293166743552/ineIFTWy_400x400.jpg"></a>
