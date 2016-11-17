f0aaf64e5087ce5359bedef87c69864106672105
mothinos <trotin.thomas@gmail.com>
2016-11-17 10:56:00 +0100

    création de la page masterclass.php et modiff du CSS

---
+++ pages/masterclass.php
@@ -0,0 +1,39 @@
+<?php require '../includes/head.php'; ?>
+	<body>
+		<?php require '../includes/header.php'; ?>
+		<div id="masterclass">
+			<h1>Master Class et rencontres</h1>
+				<ul>
+					<a href="#ludovic"><li>Ludovic Arnold</li></a>
+					<a href="#Marine"><li>Marine Dautrème</li></a>
+<div id="ludovic">
+<a href="https://twitter.com/NkL4"><img src="https://pbs.twimg.com/profile_images/770207539296436224/p6s80Pzh_400x400.jpg"></a>
+		<ul>
+			<li>Ludovic Arnold</li>
+			<li>fonction: Fondateur/bienfaiteur</li>
+			<li>Organisme:
+				<ul>
+			 		<li>La:Matrice</li>
+			 		<li>Kreizenn Dafar</li>
+			 	</ul>
+			 </li>
+			 <li>rencontre du 4 Novembre 2016</li>
+		</ul>
+		<h2>Ludovic Arnorld ou comment je suis devenu une <strong>méduse</strong> (:~</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+	</ul>
+</div>
+
+
+
+
+		</div>
+	</body>
+	<?php require '../includes/footer.php'; ?>
