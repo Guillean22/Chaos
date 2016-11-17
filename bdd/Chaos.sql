7159b0ca4561a8319a0ef3e54f4113879254a7bb
Guillean22 <jean.rodrigue.stella@gmail.com>
2016-11-17 14:54:54 +0100

    add Chaos.sql in rep bdd

---
+++ bdd/Chaos.sql
@@ -0,0 +1,7 @@
+/*BDD DU CHAOS*/
+
+CREATE TABLE SOFT{
+	id_soft int(10) auto-increment NOT NULL;
+	nom_soft varchar;
+	PRIMARY KEY(id_soft);
+}
--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 	<html>
+		<meta charset="utf-8">
 		<link rel="stylesheet" href="/Projets/Chaos/style2.css">	<!-- création du lien vers la page css -->
 		<head>
 			<title>Project Chaos</title>
