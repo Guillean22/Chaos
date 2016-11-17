3013becf8bb914b7284fd2d20cc666004988f165
xandir59 <dylan.eng@live.fr>
2016-11-17 15:00:25 +0100

    Modif tutoriaux.php

--- pages/tutoriaux.php
+++ pages/tutoriaux.php
@@ -15,9 +15,21 @@
             	 		<p>- Sélection multiple (ex : faire plusieur modifications en une fois).
             	 		   - Mode d'affichage multiple.
             	 		   - Une colonne à droite permettant de se repérer sur la page.
-            	 		   -Une palette de commande permettant d'accéder à de nombreuses fonctionnalités, snippets (configurer les préférences, insérer des portions ou des structures de code</p>
-					<li>Inconvenients</li>
+            	 		   - Une palette de commande permettant d'accéder à de nombreuses fonctionnalités, snippets (configurer les préférences,    insérer des portions ou des structures de code</p>
+					<li>Inconvénients</li>
+                        <p> Il n'est pas gratuit, on peut toutefois l'utiliser sans licence avec l'affichage r"gulier d'une popup de notification demandant de passer à la version payante.</p>
+                    <h3> Brackets</h3>
+                    <li> Principaux Avantage</li>
+                        <p>- Edition rapide : Brackets permet d'éditer les styles directement dans le fichier HTML, évitant ainsi d'ouvrir plusieur      fichier liés et de passer continuellement des uns des autres.
+                           - Aperçu en direct.
+                           - Grande sélection de thèmes et de plugins.
+                           - Prévisualiseur natif d’images et de couleurs.</p>
+                    <li>Inconvénients</li>
+                        <p>L'aperçu en direct ne fonctionne qu'avec Chrome, Le plugin LiveReload permet toutefois d'obtenir l'équivalent sur Firefox ou Safaris</p>
+                    <h4>Autre éditeur de text</h4>
+                        <p> Atome, Notepad++ </p>
             <ul>Les tutoriaux sur les languages</ul>
+            
             
         </div>
         
