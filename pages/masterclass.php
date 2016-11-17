713e7e4ff3b240981c28a45488dd469cf4f275d0
mothinos <trotin.thomas@gmail.com>
2016-11-17 12:28:41 +0100

    modif avant midi

--- pages/masterclass.php
+++ pages/masterclass.php
@@ -8,6 +8,7 @@
 					<a href="#Marine"><li>Marine Dautrème</li></a>
 <div id="ludovic">
 <a href="https://twitter.com/NkL4"><img src="https://pbs.twimg.com/profile_images/770207539296436224/p6s80Pzh_400x400.jpg"></a>
+<img title="Matrice" src="http://www.la-matrice.org/">
 		<ul>
 			<li>Ludovic Arnold</li>
 			<li>fonction: Fondateur/bienfaiteur</li>
@@ -19,14 +20,14 @@
 			 </li>
 			 <li>rencontre du 4 Novembre 2016</li>
 		</ul>
-		<h2>Ludovic Arnorld ou comment je suis devenu une <strong>méduse</strong> (:~</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+				<h2>Ludovic Arnorld ou comment je suis devenu une <strong>méduse</strong> (:~</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p>
 
 	</ul>
 	</br>
@@ -34,16 +35,16 @@
 <a href="https://twitter.com/marinedautreme"><img src="https://pbs.twimg.com/profile_images/798157293166743552/ineIFTWy_400x400.jpg"></a>
 		<ul>
 			<li>Marine Dautrème</li>
-			<li>fonction: relatin avec des employeurs</li>
+			<li>fonction: relation avec des employeurs</li>
 			<li>Organisme:
 				<ul>
-			 		<li>Fondation agissons pour l'emploi</li>
-			 		<!--<li>Kreizenn Dafar</li>-->
+			 		<a href="http://www.agissonspourlemploi.fr/"><li>Fondation agissons pour l'emploi</li></a>
+			 		<a href="http://www.fondationface.org/"><li>Fondation agissons contre exclusion</li></a>
 			 	</ul>
 			 </li>
 			 <li>rencontre du 10 Novembre 2016</li>
 		</ul>
-		<h2>Marine Dautrème ou comment entrer en realation avec des <strong>employeur</strong> (:~</h2>
+		<h2>Marine Dautrème ou comment entrer en relation avec des <strong>employeurs</strong></h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
