6140d885e123d04fcbf74a9a209caa40388d9576
mothinos <trotin.thomas@gmail.com>
2016-11-17 12:04:30 +0100

    ajout dans masterclass.php

--- pages/masterclass.php
+++ pages/masterclass.php
@@ -29,6 +29,30 @@
 
 
 	</ul>
+	</br>
+	<div id="Marine">
+<a href="https://twitter.com/marinedautreme"><img src="https://pbs.twimg.com/profile_images/798157293166743552/ineIFTWy_400x400.jpg"></a>
+		<ul>
+			<li>Marine Dautrème</li>
+			<li>fonction: relatin avec des employeurs</li>
+			<li>Organisme:
+				<ul>
+			 		<li>Fondation agissons pour l'emploi</li>
+			 		<!--<li>Kreizenn Dafar</li>-->
+			 	</ul>
+			 </li>
+			 <li>rencontre du 10 Novembre 2016</li>
+		</ul>
+		<h2>Marine Dautrème ou comment entrer en realation avec des <strong>employeur</strong> (:~</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+	</ul>
 </div>
 
 
