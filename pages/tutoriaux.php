fd2ab5c138d9f5ef16923f117f513f4e0ce2dca0
xandir59 <dylan.eng@live.fr>
2016-11-17 13:26:24 +0100

    modif tuto.php

--- pages/tutoriaux.php
+++ pages/tutoriaux.php
@@ -9,6 +9,14 @@
 		</div>
         <div class=list>
             <ul>Les tutoriaux logiciels </ul> 
+            	<h2>Comparatif</h2>
+            	 <h3>Sublime text 3</h3>
+            	 	<li>Principaux Avantage</li>
+            	 		<p>- Sélection multiple (ex : faire plusieur modifications en une fois).
+            	 		   - Mode d'affichage multiple.
+            	 		   - Une colonne à droite permettant de se repérer sur la page.
+            	 		   -Une palette de commande permettant d'accéder à de nombreuses fonctionnalités, snippets (configurer les préférences, insérer des portions ou des structures de code</p>
+					<li>Inconvenients</li>
             <ul>Les tutoriaux sur les languages</ul>
             
         </div>
