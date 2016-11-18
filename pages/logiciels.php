68606a46fa570dd2d609c85daf5720de552989f3
xandir59 <dylan.eng@live.fr>
2016-11-18 08:29:03 +0100

    creation page logiciel.php

---
+++ pages/logiciels.php
@@ -0,0 +1,35 @@
+<?php require '../includes/head.php'; ?>
+<link rel="stylesheet" href="/Projets/Chaos/tutoriaux.css">	
+
+	<body>
+		<?php require '../includes/header.php'; ?>
+		<div>
+            <h1>Tutoriaux des logiciels</h1>
+        </div>
+        <div>
+             <a href="#" onmouseover="document.getElementById('test').style.display='block';" onmouseout="document.getElementById('test').style.display='none';">Sublime text 3</a>
+                <div id="test" style="display:none; width:500px; height:500px; background-color:withe;">
+                    <h3>Sublime text 3</h3>
+                        <li>Principaux Avantage</li>
+                            <p>- Sélection multiple (ex : faire plusieur modifications en une fois).
+                                   - Mode d'affichage multiple.
+                                   - Une colonne à droite permettant de se repérer sur la page.
+                                   - Une palette de commande permettant d'accéder à de nombreuses fonctionnalités, snippets (configurer les préférences,    insérer des portions ou des structures de code</p>
+                        <li>Inconvénients</li>
+                            <p> Il n'est pas gratuit, on peut toutefois l'utiliser sans licence avec l'affichage r"gulier d'une popup de notification demandant de passer à la version payante.</p>
+        </div>
+        <div>
+            <a href="#" onmouseover="document.getElementById('test').style.display='block';" onmouseout="document.getElementById('test').style.display='none';">Brackets</a>
+                <div id="test" style="display:none; width:500px; height:500px; background-color:withe;">
+                    <h3>Brackets</h3>
+                        <li> Principaux Avantage</li>
+                            <p>- Edition rapide : Brackets permet d'éditer les styles directement dans le fichier HTML, évitant ainsi d'ouvrir plusieur      fichier liés et de passer continuellement des uns des autres.
+                                   - Aperçu en direct.
+                                   - Grande sélection de thèmes et de plugins.
+                                   - Prévisualiseur natif d’images et de couleurs.</p>
+                         <li>Inconvénients</li>
+                            <p>L'aperçu en direct ne fonctionne qu'avec Chrome, Le plugin LiveReload permet toutefois d'obtenir l'équivalent sur Firefox ou Safaris</p>
+        </div>
+        
+	</body>
+	<?php require '../includes/footer.php'; ?>
