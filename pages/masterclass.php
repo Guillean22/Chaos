8c243dd0c386a85e0b6fe5cc3836faae253dbfa5
mothinos <trotin.thomas@gmail.com>
2016-11-18 12:21:31 +0100

    pull du vendredi midi

--- pages/masterclass.php
+++ pages/masterclass.php
@@ -6,67 +6,67 @@
 				<ul>
 					<a href="#ludovicarnold"><li>Ludovic Arnold</li></a>
 					<a href="#marinedautreme"><li>Marine Dautrème</li></a>
-<div id="ludovicarnold">
-	<a href="https://twitter.com/NkL4"><img src="https://pbs.twimg.com/profile_images/770207539296436224/p6s80Pzh_400x400.jpg" alt="ludovicarnold" class="photopresentation"></a>
-		<img title="Matrice" src="http://www.la-matrice.org/wp-content/uploads/2013/05/logo-icononly.png" alt="logo_matrice" title="matrice">
-			<article>
-				<ul class="presentation">
-					<li>Ludovic Arnold</li>
-					<li>fonction: Fondateur/bienfaiteur</li>
-					<li>Organisme:
-						<ul>
-					 		<li>La:Matrice</li>
-					 		<li>Kreizenn Dafar</li>
-						</ul>
-					 </li>
-					 <li>rencontre du 4 Novembre 2016</li>
-				</ul>
-						<h2>Ludovic Arnorld ou comment je suis devenu une <strong>méduse</strong> (:~</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						<h2>Renseignez-vous!</h2>
-							<p>DATALOVE <3</p>
-							<P><a href="https://en.wikipedia.org/wiki/Real_life">Meat Space</a></P>
-							<p>telecomix</p>
-							<p>Benjamin Bayar: <a href="https://www.youtube.com/watch?v=AoRGoQ76PK8">minitel 2.0</a></p>
-							<p>Fidh</p>
-							<p>Fabrice Epelboin</p>
-							<p>LeanStartup</p>
-							<p>BMC</p>
-							<p>la quadrature du net</p>
-							<p>méthode agile</p>
-							<p>pas sage en seine</p>
-				</ul>
-		</article>
-	</br>
-	</div>
-	<div id="marinedautreme">
-<a href="https://twitter.com/marinedautreme"><img src="https://pbs.twimg.com/profile_images/798157293166743552/ineIFTWy_400x400.jpg" alt="marinedautreme" class="photopresentation"></a>
-		<ul class="presentation">
-			<li>Marine Dautrème</li>
-			<li>fonction: relation avec des employeurs</li>
-			<li>Organisme:
-				<ul>
-			 		<li><a href="http://www.agissonspourlemploi.fr/">Fondation agissons pour l'emploi</a></li>
-			 		<li><a href="http://www.fondationface.org/">Fondation agissons contre exclusion</a></li>
-			 	</ul>
-			 </li>
-			 <li>rencontre du 10 Novembre 2016</li>
-		</ul>
-		<h2>Marine Dautrème ou comment entrer en relation avec des <strong>employeurs</strong></h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<div id="ludovicarnold">
+							<a href="https://twitter.com/NkL4"><img src="https://pbs.twimg.com/profile_images/770207539296436224/p6s80Pzh_400x400.jpg" alt="ludovicarnold" class="photopresentation"></a>
+									<img title="Matrice" src="http://www.la-matrice.org/wp-content/uploads/2013/05/logo-icononly.png" alt="logo_matrice" title="matrice">
+										<article>
+											<ul class="presentation">
+												<li>Ludovic Arnold</li>
+												<li>fonction: Fondateur/bienfaiteur</li>
+												<li>Organisme:
+													<ul>
+												 		<li>La:Matrice</li>
+												 		<li>Kreizenn Dafar</li>
+													</ul>
+												 </li>
+												 <li>rencontre du 4 Novembre 2016</li>
+											</ul>
+													<h2>Ludovic Arnorld ou comment je suis devenu une <strong>méduse</strong> (:~</h2>
+														<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+														tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+														quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+														consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+														cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+														proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+													<h2>Renseignez-vous!</h2>
+														<p>DATALOVE <3</p>
+														<P><a href="https://en.wikipedia.org/wiki/Real_life">Meat Space</a></P>
+														<p>telecomix</p>
+														<p>Benjamin Bayar: <a href="https://www.youtube.com/watch?v=AoRGoQ76PK8">minitel 2.0</a></p>
+														<p>Fidh</p>
+														<p>Fabrice Epelboin</p>
+														<p>LeanStartup</p>
+														<p>BMC</p>
+														<p>la quadrature du net</p>
+														<p>méthode agile</p>
+														<p>pas sage en seine</p>
+											</ul>
+									</article>
+								</br>
+								</div>
+								<div id="marinedautreme">
+							<a href="https://twitter.com/marinedautreme"><img src="https://pbs.twimg.com/profile_images/798157293166743552/ineIFTWy_400x400.jpg" alt="marinedautreme" class="photopresentation"></a>
+									<ul class="presentation">
+										<li>Marine Dautrème</li>
+										<li>fonction: relation avec des employeurs</li>
+										<li>Organisme:
+											<ul class="presentation">
+										 		<li><a href="http://www.agissonspourlemploi.fr/">Fondation agissons pour l'emploi</a></li>
+										 		<li><a href="http://www.fondationface.org/">Fondation agissons contre exclusion</a></li>
+										 	</ul>
+										 </li>
+										 <li>rencontre du 10 Novembre 2016</li>
+									</ul>
+									<h2>Marine Dautrème ou comment entrer en relation avec des <strong>employeurs</strong></h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+									proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
 
-	</ul>
+								</ul>
 </div>
 
 </body>
